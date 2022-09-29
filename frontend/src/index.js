3e861a35d6ead00cd0a6fe318afedc58f2ba4aaa
Vlad <vlader101@gmail.com>
2022-09-29 15:19:35 -0500

    Add JoyUI frontend dependencies.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { StyledEngineProvider, CssVarsProvider } from "@mui/joy/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <StyledEngineProvider injectFirst>
+            <CssVarsProvider>
+                <App />
+            </CssVarsProvider>
+        </StyledEngineProvider>
     </React.StrictMode>
 );
