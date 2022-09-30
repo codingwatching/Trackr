09f1177f67a3ba9c098a880ac928a61093365271
Vlad <vlader101@gmail.com>
2022-09-30 18:14:20 -0500

    Revert to using Material library rather than Joy library.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,69 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
-import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Card from "@mui/joy/Card";
-import IconButton from "@mui/joy/IconButton";
-import Typography from "@mui/joy/Typography";
-import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
+import Card from "@mui/material/Card";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 
-const BasicCard = () => {
-    return (
-        <Card variant="outlined" sx={{ maxWidth: "320px", margin: "0 auto" }}>
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 0.5 }}>
-                <Typography
-                    level="h2"
-                    fontSize="md"
-                    sx={{ alignSelf: "flex-start" }}
-                >
-                    Yosemite National Park
-                </Typography>
-                <Typography level="body2">April 24 to May 02, 2021</Typography>
-            </Box>
-            <IconButton
-                aria-label="bookmark Bahamas Islands"
-                variant="plain"
-                color="neutral"
-                size="sm"
-                sx={{ position: "absolute", top: "0.5rem", right: "0.5rem" }}
-            >
-                <BookmarkAdd />
-            </IconButton>
-
-            <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
-                <img
-                    src="https://images.unsplash.com/photo-1527549993586-dff825b37782?crop=entropy&auto=format&fit=crop&w=3270"
-                    alt=""
-                />
-            </AspectRatio>
-            <Box sx={{ display: "flex" }}>
-                <div>
-                    <Typography level="body3">Total price:</Typography>
-                    <Typography fontSize="lg" fontWeight="lg">
-                        $2900
-                    </Typography>
-                </div>
-                <Button
-                    variant="solid"
-                    size="sm"
-                    color="primary"
-                    aria-label="Explore Bahamas Islands"
-                    sx={{ ml: "auto", fontWeight: 600 }}
-                >
-                    Explore
-                </Button>
-            </Box>
-        </Card>
-    );
+const BasicList = () => {
+  return (
+    <Card
+      sx={{
+        width: "100%",
+        m: "0 auto",
+        maxWidth: 360,
+        bgcolor: "background.paper",
+      }}
+    >
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Card>
+  );
 };
 
 const App = () => {
-    return (
-        <>
-            <BasicCard />
-        </>
-    );
+  return (
+    <>
+      <BasicList />
+    </>
+  );
 };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { StyledEngineProvider, CssVarsProvider } from "@mui/joy/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <StyledEngineProvider injectFirst>
-            <CssVarsProvider>
-                <App />
-            </CssVarsProvider>
-        </StyledEngineProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
