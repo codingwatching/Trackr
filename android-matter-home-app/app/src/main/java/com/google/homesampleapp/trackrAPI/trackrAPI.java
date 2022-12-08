99f6e48b8b2a3fe0e42874df38f9cf8b1d77b5f9
Kunal <rajpal.kunal1@gmail.com>
2022-12-07 18:19:47 -0600

    Post

--- android-matter-home-app/app/src/main/java/com/google/homesampleapp/trackrAPI/trackrAPI.java
+++ android-matter-home-app/app/src/main/java/com/google/homesampleapp/trackrAPI/trackrAPI.java
@@ -1,16 +1,25 @@
 package com.google.homesampleapp.trackrAPI;
 
 
-import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class trackrAPI {
+
+    //variables
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
 
 
     public int login(){
@@ -33,16 +42,15 @@ public class trackrAPI {
         //http
     }
 
-    private void post()
-        throws IOException{
-            OkHttpClient client = new OkHttpClient();
-            String url = "";
+    private String post(String url, String payload) throws IOException{
 
+            RequestBody body = RequestBody.create(JSON, payload);
             Request request = new Request.Builder()
                     .url(url)
+                    .post(body)
                     .build();
-
-            Call call = client.newCall(request);
-            Response response = call.execute();
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            }
     }
 }
