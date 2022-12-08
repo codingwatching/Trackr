1e876bcfede7d38d820a2a71e67b45c55ae682a7
Kunal <rajpal.kunal1@gmail.com>
2022-12-07 21:24:01 -0600

    Added comments

--- android-matter-home-app/app/src/main/java/com/google/homesampleapp/trackrAPI/trackrAPI.java
+++ android-matter-home-app/app/src/main/java/com/google/homesampleapp/trackrAPI/trackrAPI.java
@@ -11,19 +11,18 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class trackrAPI {
 
     //variables
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
-
-
 
     public int login(){
         //http
@@ -44,8 +43,51 @@ public class trackrAPI {
     public void sendData(){
         //http
     }
+//------------------------------------------------------
+//GET
+//INPUT PARAMETERS: address for the GET request
+//RETURN: response as a string
+//------------------------------------------------------
+    private String get(String url) throws IOException{
 
+        String retVal = "";
 
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try(ResponseBody responseBody = response.body()) {
+
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+
+                    for (int i=0;  i < responseHeaders.size(); i++){
+                        Log.i(responseHeaders.name(i), responseHeaders.value(i));
+                    }
+                }
+            }
+        });
+
+        return retVal;
+    }
+
+//------------------------------------------------------
+//Post
+//INPUT PARAMETERS: url (String): address for the POST request
+//                  payload (String): msg to be posted
+//RETURN: response (String)
+//------------------------------------------------------
     private String post(String url, String payload) throws IOException{
 
         String retVal = "";
@@ -60,13 +102,20 @@ public class trackrAPI {
         try (Response response = client.newCall(request).execute()) {
             retVal = response.body().string();
         } catch (Exception e) {
-            Log.i("Post Reponse", "Error while posting request to the client");
+            Log.i("Post Response", "Error while posting request to the client");
             retVal = "Error";
         }
 
         return retVal;
     }
 
+//------------------------------------------------------
+//Post with Header
+//INPUT PARAMETERS: url (String): address for the POST request
+//                  payload (String): msg to be posted
+//                  head (String): Value for the header
+//RETURN: response (String)
+//------------------------------------------------------
     private String post (String url, String payload, String head) throws IOException{
 
         String retVal = "";
