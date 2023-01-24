050d67750091318472e574d5001e44f60174d1bc
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-01-24 15:35:11 -0600

    .history removal
    added .history to ,gitignore so that local history files are not added to the repo
    .history folder deleted from repository

--- .history/frontend/src/pages/Organisations_20230118212957.js
+++
@@ -1,8 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-// Page that you can open through the navbar
-// Page includes a list of organisations currently created
-
-const Organisations = () => {
-  return ()
-};
