25341fb93da6dba6eec335f5a260a21f0d257619
BrettDowneyuofm <84928888+BrettDowneyuofm@users.noreply.github.com>
2023-04-04 02:09:12 -0500

    Uncomment project ID when creating log

--- backend/src/services_impl/db/logs.go
+++ backend/src/services_impl/db/logs.go
@@ -27,7 +27,7 @@ func (service *LogService) AddLog(message string, user models.User, projectId *u
 	log := models.Log{
 		Message:   message,
 		User:      user,
-		// ProjectID: projectId,
+		ProjectID: projectId,
 	}
 
 	if result := service.DB.Create(&log); result.Error != nil {
