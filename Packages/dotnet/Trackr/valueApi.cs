8e92c44d766109b91325bd42d947dcd536e8ab6a
Cody Wallbridge <codywallbridge@gmail.com>
2023-06-19 22:21:11 -0500

    removed extra comment, added documentation comments

--- Packages/dotnet/Trackr/valueApi.cs
+++ Packages/dotnet/Trackr/valueApi.cs
@@ -13,6 +13,13 @@ namespace Trackr
         private static readonly HttpClient client = new HttpClient();
         private static string ApiEndpoint = "http://wryneck.cs.umanitoba.ca:3000/values";
 
+        /// <summary>
+        /// Add's a single value to the field of the project which has the apikey.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="fieldId"></param>
+        /// <param name="value"></param>
+        /// <returns>Returns an HttpResponseMessage with a statusCode and Content detailing the result of the operation.</returns>
         static async Task<HttpResponseMessage> AddSingleValue(string apiKey, uint fieldId, string value)
         {
             var valueObject = new Dictionary<string, string>
@@ -27,9 +34,15 @@ namespace Trackr
             return response;
         }
 
+        /// <summary>
+        /// Accepts a list of string values to be added to the field of the project which has the apiKey.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="fieldId"></param>
+        /// <param name="values"></param>
+        /// <returns>Returns an HttpResponseMessage with a statusCode and Content detailing the result of the operation.</returns>
         static async Task<HttpResponseMessage> AddManyValues(string apiKey, uint fieldId, List<string> values)
         {
-            //look into rate limiting, figure out how to handle it
             foreach (var value in values)
             {
                 HttpResponseMessage result = await AddSingleValue(apiKey, fieldId, value);
@@ -42,6 +55,15 @@ namespace Trackr
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
+        /// <summary>
+        /// Accepts configuration parameters which target a project which has the apiKey, to read in "limit" number of values from the field with "fieldId", starting at the "offset" value. Acceptable orders are "asc" or "desc".
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="fieldId"></param>
+        /// <param name="offset"></param>
+        /// <param name="limit"></param>
+        /// <param name="order"></param>
+        /// <returns>Returns an HttpResponseMessage with a statusCode and Content detailing the result of the operation. If the operation was successful, the content will contain "totalValues" which is a number and "values" which is a list of the values returned.</returns>
         static async Task<HttpResponseMessage> GetValues(string apiKey, uint fieldId, uint offset, int limit, string order)
         {
             if (!order.ToLower().Equals("asc") && !order.ToLower().Equals("desc"))
