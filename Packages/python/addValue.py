e85a4cc38fb200751f2b7b4e2dfe9e506861fc36
Cody Wallbridge <codywallbridge@gmail.com>
2023-06-26 13:19:43 -0500

    removed excess files

--- Packages/python/addValue.py
+++
@@ -1,5 +0,0 @@
-class addValue :
-    def __init__(apiKey, value, fieldId):
-        self.ApiKey = apiKey
-        self.Value = value
-        self.FieldId = fieldId
--- Packages/python/getValues.py
+++
@@ -1,7 +0,0 @@
-class getValues:
-    def __init__(apiKey, value, offset, limit, order):
-        self.ApiKey = apiKey
-        self.Value = value
-        self.Offset = offset
-        self.Limit = limit
-        self.Order = order
