14bd9fc4c840d82b8dece8a32fe1899af2e57ea0
Ethan Ducharme <ethanducharm@gmail.com>
2023-07-08 21:32:41 -0500

    Finished the bash scripting funcs

--- Packages/bash/trackr.sh
+++ Packages/bash/trackr.sh
@@ -1,3 +1,11 @@
+# How to use:
+#
+# trackr.sh add [field] [value]
+# trackr.sh add-batch [field] [values...]
+# trackr.sh get [field] [offset] [limit] [order]
+#
+# Set TRACKR_API_KEY environment variable to your API key. 
+# You can override the default host by setting TRACKR_API_HOST environment variable
 
 ApiEndpoint="http://wryneck.cs.umanitoba.ca/api/values"
 
@@ -11,9 +19,10 @@ function AddSingleValue() {
         exit 1
     fi
 
-    local data='{"apiKey":"'"$apiKey"'","value":"'"$value"'","fieldId":'"$fieldId"'}'
-    response=$(curl -s -X POST "$ApiEndpoint" --header "Content-Type: application/json" --data "$data")
-    echo "$response"
+    local data="apiKey=$apiKey&value=$value&fieldId=$fieldId"
+    response=$(curl -L -s -X POST "$ApiEndpoint" --header "Content-Type: application/x-www-form-urlencoded" --data "$data")
+    
+    echo $response
 }
 
 function AddManyValues() {
@@ -52,6 +61,26 @@ function GetValues() {
     fi
 
     local params="apiKey=$apiKey&fieldId=$fieldId&offset=$offset&limit=$limit&order=$order"
-    response=$(curl -s -X GET "$ApiEndpoint?$params")
+    response=$(curl -L -s -X GET "$ApiEndpoint?$params")
     echo "$response"
 }
+
+if [ -z "${TRACKR_API_KEY}" ]; then
+    echo "API key is unset. Please set TRACKR_API_KEY environment variable"
+    exit 0
+fi
+    
+if [ -n "${TRACKR_API_HOST}" ]; then
+    ApiEndpoint = "${TRACKR_API_HOST}"
+fi
+
+
+if [[ "$1" = "add" ]]; then
+    AddSingleValue "${TRACKR_API_KEY}" "$2" "$3"
+elif [[ "$1" = "add-batch" ]]; then
+    AddManyValues "${TRACKR_API_KEY}" "$2" "${@:3}"
+elif [[ "$1" = "get" ]]; then
+    GetValues "${TRACKR_API_KEY}" "$2" "$3" "$4" "$5"
+else
+    echo "Unknown command"
+fi
